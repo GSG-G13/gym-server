cd56fb6a7cb132de7c95d57af8c16c6204c2b112
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-06-13 12:04:56 +0300

    fix mongodb connection Relate #8 closes #8

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import server from './app'
 import { PORT, DB_URL } from './config'
 import mongoose from 'mongoose'
 
-mongoose.connect(DB_URL)
-  .then(() => server.listen(PORT, () => { console.log('SERVER IS RUNNING!') }))
-  .catch(err => { console.log(err) })
+server.listen(PORT, async ()=>{
+  console.log('SERVER IS RUNNING!')
+
+  await mongoose.connect(DB_URL)
+})
