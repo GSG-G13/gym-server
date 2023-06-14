75b723bafcdbc3cc4e7f5efbde6b6c6e62ca7a98
abeerAyad <abeerayyad2000@gmail.com>
2023-06-14 16:53:48 +0300

    checking product is existing or not Relates#5

--- src/controllers/products/addProduct.ts
+++ src/controllers/products/addProduct.ts
@@ -4,6 +4,10 @@ import { Product } from '../../database/productSchema';
 const addProduct = async (req: Request, res: Response): Promise<void> => {
   try {
     const { title, image, rating, price } = req.body;
+    const isExistedProduct = await Product.findOne({ title });
+    if (isExistedProduct) {
+      res.status(400).json({ msg: 'Product Is Existing'});
+    }
     await Product.create({
       title,
       image,
