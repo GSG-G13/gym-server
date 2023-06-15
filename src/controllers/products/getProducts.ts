8d8f4e9e37e430d2da8dc0347e1ff5c21edbfd73
abeerAyad <abeerayyad2000@gmail.com>
2023-06-15 13:33:32 +0300

    add custom error

--- src/controllers/products/getProducts.ts
+++ src/controllers/products/getProducts.ts
@@ -1,12 +1,13 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import Product from '../../database/productSchema';
+import CustomError from '../../helpers';
 
-const getProducts = async (req: Request, res: Response): Promise<void> => {
+const getProducts = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const products = await Product.find();
     res.status(200).json({ products, msg: 'Get Products Successfully' });
   } catch (error) {
-    res.status(400).json({ error });
+    next(new CustomError(400, error.message));
   }
 };
 
