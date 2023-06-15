6d078af39caaf283dbd574759057862398540f67
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-06-15 13:37:49 +0300

    get announcements Rleate #29

---
+++ src/controllers/announcement/getannouncements.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import Announcement from '../../database/announcement';
+import CustomError from '../../helpers';
+
+const getAnnouncements = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const announcements = await Announcement.find();
+    res.status(200).json({
+      announcements,
+      msg: 'Get Announcements Successfully',
+    });
+  } catch (error) {
+    next(new CustomError(400, error.message));
+  }
+};
+
+export default getAnnouncements;
