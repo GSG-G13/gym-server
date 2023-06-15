a207553fb21e1257c7c31a48636ace091f7aedd6
heleena-fadi <miss.heleena98@gmail.com>
2023-06-15 12:26:24 +0300

    add checkRole to class Relates #6 Closes #6

--- src/routers/class.ts
+++ src/routers/class.ts
@@ -2,9 +2,10 @@ import express, { type Router } from 'express';
 import addClass from '../controllers/classes/addClass';
 import validationData from '../middlewares/validation';
 import classesValidationSchema from '../validations/classesValidationSchema';
+import checkRole from '../middlewares/checkRole';
 
 const classRouter: Router = express.Router();
 
-classRouter.post('/', validationData(classesValidationSchema), addClass);
+classRouter.post('/', checkRole, validationData(classesValidationSchema), addClass);
 
 export default classRouter;
