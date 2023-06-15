91ab64700b563ce1707bd3c2956a21d1b44654b1
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-06-15 16:52:27 +0300

    update announcement Relate #40

---
+++ src/controllers/announcement/updateacnnouncement.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+import Announcement from '../../database/announcement';
+import CustomError from '../../helpers';
+
+const updateAnnouncement = async (
+  req:Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { title } = req.body;
+    const { id } = req.params;
+    const updatedAnnouncement = await Announcement.findByIdAndUpdate(id, { title });
+    res.status(203).json({
+      updatedAnnouncement,
+      msg: 'announcement has been updated successfully',
+    });
+  } catch (error) {
+    next(new CustomError(500, error.message));
+  }
+};
+
+export default updateAnnouncement;
