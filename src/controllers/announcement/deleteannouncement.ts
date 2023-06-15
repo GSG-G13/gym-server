75625782ff2888feebbd3cca4dc46ccaf4a63578
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-06-15 16:22:15 +0300

    delete annoouncement Relate #35

---
+++ src/controllers/announcement/deleteannouncement.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+import Announcement from '../../database/announcement';
+import CustomError from '../../helpers';
+
+const deleteAnnouncement = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const deletedAnnouncement = await Announcement.deleteOne({ _id: id });
+
+    res.status(200).json({
+      deletedAnnouncement,
+      msg: 'this announcement has been deleted successfully',
+    });
+  } catch (error) {
+    next(new CustomError(500, error.message));
+  }
+};
+
+export default deleteAnnouncement;
--- src/routers/announcement/index.ts
+++ src/routers/announcement/index.ts
@@ -4,10 +4,11 @@ import validationData from '../../middlewares/validation';
 import announcementValidation from '../../validations/announcement';
 import checkRole from '../../middlewares/checkRole';
 import getAnnouncements from '../../controllers/announcement/getannouncements';
+import deleteAnnouncement from '../../controllers/announcement/deleteannouncement';
 
 const announcementRouter: Router = express.Router();
 
 announcementRouter.post('/', checkRole, validationData(announcementValidation), addAnnouncement);
 announcementRouter.get('/', getAnnouncements);
-
+announcementRouter.delete('/:id', deleteAnnouncement);
 export default announcementRouter;
