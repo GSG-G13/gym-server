f06e6a4530d1b16a9600cbff2b8ce0d4eb418fa8
abeerAyad <abeerayyad2000@gmail.com>
2023-06-15 12:20:19 +0300

    add check role Relates#5

--- src/routers/product.ts
+++ src/routers/product.ts
@@ -2,9 +2,10 @@ import express from 'express';
 import productValidationSchema from '../validations/productValidationSchema';
 import validationData from '../middlewares/validation';
 import { addProduct } from '../controllers/products';
+import checkRole from '../middlewares/checkRole';
 
 const productRouter = express.Router();
 
-productRouter.post('/', validationData(productValidationSchema), addProduct);
+productRouter.post('/', checkRole, validationData(productValidationSchema), addProduct);
 
 export default productRouter;
