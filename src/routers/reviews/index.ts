791a11d4a4b0f0dbc119640ca9cf888bfd41d05e
AhmedMughessib <amoghaseeb@gmail.com>
2023-06-16 19:19:09 +0300

    get reviews by product ID

--- src/routers/reviews/index.ts
+++ src/routers/reviews/index.ts
@@ -1,9 +1,11 @@
 import express, { type Router } from 'express';
 import addReview from '../../controllers/reviews/addreview';
 import checkAuth from '../../middlewares/checkAuth';
+import getReviews from '../../controllers/reviews/getreviews';
 
 const reviewRouter: Router = express.Router();
 
 reviewRouter.post('/', checkAuth, addReview);
+reviewRouter.get('/:id', getReviews);
 
 export default reviewRouter;
