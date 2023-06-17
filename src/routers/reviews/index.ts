8f4c3f480d97131df57caad0bd30a977605caa72
AhmedMughessib <amoghaseeb@gmail.com>
2023-06-17 12:50:45 +0300

    delte a product by product id and user id

--- src/routers/reviews/index.ts
+++ src/routers/reviews/index.ts
@@ -1,9 +1,11 @@
 import express, { type Router } from 'express';
 import addReview from '../../controllers/reviews/addreview';
 import checkAuth from '../../middlewares/checkAuth';
+import deleteReview from '../../controllers/reviews/deletereview';
 
 const reviewRouter: Router = express.Router();
 
 reviewRouter.post('/', checkAuth, addReview);
+reviewRouter.delete('/', checkAuth, deleteReview);
 
 export default reviewRouter;
