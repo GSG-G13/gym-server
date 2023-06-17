811938305d586363888553b0e32a1077316dab30
heleena-fadi <miss.heleena98@gmail.com>
2023-06-17 21:08:38 +0300

    delete class controller and router Relates #42 Closes #42

--- src/controllers/classes/index.ts
+++ src/controllers/classes/index.ts
@@ -1,4 +1,5 @@
 import addClass from './addClass';
 import getClasses from './getClasses';
+import deleteClass from './deleteClass';
 
-export { addClass, getClasses };
+export { addClass, getClasses, deleteClass };
--- src/routers/class.ts
+++ src/routers/class.ts
@@ -2,11 +2,13 @@ import express, { type Router } from 'express';
 import validationData from '../middlewares/validation';
 import classesValidationSchema from '../validations/classesValidationSchema';
 import checkRole from '../middlewares/checkRole';
-import { addClass, getClasses } from '../controllers/classes';
+import { addClass, getClasses, deleteClass } from '../controllers/classes';
 
 const classRouter: Router = express.Router();
 
 classRouter.post('/', checkRole, validationData(classesValidationSchema), addClass);
 classRouter.get('/', getClasses);
+
+classRouter.delete('/:id', deleteClass);
 
 export default classRouter;
