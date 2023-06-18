66f57a242184fc5137d51bd62f64635df0224e3e
AhmedMughessib <amoghaseeb@gmail.com>
2023-06-18 12:38:36 +0300

    sign in

--- src/routers/user/index.ts
+++ src/routers/user/index.ts
@@ -2,9 +2,12 @@ import express, { type Router } from 'express';
 import signup from '../../controllers/user/signup';
 import validationData from '../../middlewares/validation';
 import { signupValidationSchema } from '../../validations/index';
+import signIn from '../../controllers/user/signin';
+import loginvalidation from '../../validations/loginvalidation';
 
 const userRouter: Router = express.Router();
 
-userRouter.post('/', validationData(signupValidationSchema), signup);
+userRouter.post('/signup', validationData(signupValidationSchema), signup);
+userRouter.post('/login', validationData(loginvalidation), signIn);
 
 export default userRouter;
