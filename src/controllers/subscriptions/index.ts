e6434e61d89d26c223389d7e317d8d28f65e463b
heleena-fadi <miss.heleena98@gmail.com>
2023-06-18 14:52:50 +0300

    get user subscription controller and router Relates #79 Closes #79

--- src/controllers/subscriptions/index.ts
+++ src/controllers/subscriptions/index.ts
@@ -1,4 +1,5 @@
 import addSubscription from './addSubscription';
 import getSubscriptions from './getSubscriptions';
+import getUserSubscriptions from './getUserSubscriptions';
 
-export { addSubscription, getSubscriptions };
+export { addSubscription, getSubscriptions, getUserSubscriptions };
