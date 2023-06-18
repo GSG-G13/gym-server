10e5ccdbbb6cefa4bf47e218e5f8ad360df50d29
AhmedMughessib <amoghaseeb@gmail.com>
2023-06-18 12:20:13 +0300

    get all users

--- src/routers/user/index.ts
+++ src/routers/user/index.ts
@@ -2,9 +2,11 @@ import express, { type Router } from 'express';
 import signup from '../../controllers/user/signup';
 import validationData from '../../middlewares/validation';
 import { signupValidationSchema } from '../../validations/index';
+import getAllUsers from '../../controllers/user/getallusers';
 
 const userRouter: Router = express.Router();
 
 userRouter.post('/', validationData(signupValidationSchema), signup);
+userRouter.get('/', getAllUsers);
 
 export default userRouter;
