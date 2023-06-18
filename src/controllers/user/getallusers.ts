88f8aebaca3c254a9c4f66e652ac6128a1cc7aed
AhmedMughessib <amoghaseeb@gmail.com>
2023-06-18 13:36:55 +0300

    fix password return

--- src/controllers/user/getallusers.ts
+++ src/controllers/user/getallusers.ts
@@ -4,7 +4,7 @@ import CustomError from '../../helpers';
 
 const getAllUsers = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
-    const allUsers = await User.find({}, { password: 0 });
+    const allUsers = await User.find({}, { password: 1 });
     res.status(200).json({ message: 'user sent successfully', allUsers });
   } catch (error) {
     next(new CustomError(500, error.message));
