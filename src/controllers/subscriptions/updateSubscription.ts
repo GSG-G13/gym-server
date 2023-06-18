351a891cb50bdfb679744a82228160893d522047
heleena-fadi <miss.heleena98@gmail.com>
2023-06-18 15:48:03 +0300

    update subscription controller and router Relates #84 Closes #84

---
+++ src/controllers/subscriptions/updateSubscription.ts
@@ -0,0 +1,20 @@
+import { NextFunction, type Request, type Response } from 'express';
+import Subscription from '../../database/subscriptionSchema';
+import CustomError from '../../helpers';
+
+const updateSubscription = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const UpdateSubData = await Subscription.findByIdAndUpdate({ _id: id }, { status: 'joined' });
+    res.status(200).json({ UpdateSubData, msg: 'Deleted successfully!' });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    next(new CustomError(500, error.message));
+  }
+};
+
+export default updateSubscription;
