cc86c8668fc333f0c38989fce48970d752ec28ca
heleena-fadi <miss.heleena98@gmail.com>
2023-06-18 16:15:55 +0300

    delete subscription controller and router Relates #86 Closes #86

--- src/controllers/subscriptions/index.ts
+++ src/controllers/subscriptions/index.ts
@@ -1,4 +1,5 @@
 import addSubscription from './addSubscription';
 import getSubscriptions from './getSubscriptions';
+import deleteSubscription from './deleteSubscription';
 
-export { addSubscription, getSubscriptions };
+export { addSubscription, getSubscriptions, deleteSubscription };
--- src/routers/subscription.ts
+++ src/routers/subscription.ts
@@ -1,10 +1,12 @@
 import express, { type Router } from 'express';
 import checkAuth from '../middlewares/checkAuth';
-import { addSubscription, getSubscriptions } from '../controllers/subscriptions';
+import { addSubscription, getSubscriptions, deleteSubscription } from '../controllers/subscriptions';
+import checkRole from '../middlewares/checkRole';
 
 const SubscriptionRouter: Router = express.Router();
 
 SubscriptionRouter.post('/:classId', checkAuth, addSubscription);
 SubscriptionRouter.get('/', getSubscriptions);
+SubscriptionRouter.delete('/:id', checkRole, deleteSubscription);
 
 export default SubscriptionRouter;
