31e872299b32308faf0258bc915123661a7f5788
heleena-fadi <miss.heleena98@gmail.com>
2023-06-18 15:17:08 +0300

    fixing

---
+++ src/controllers/subscriptions/getUserSubscriptions.ts
@@ -0,0 +1,21 @@
+import { NextFunction, type Response } from 'express';
+import Subscription from '../../database/subscriptionSchema';
+import CustomError from '../../helpers';
+import { UserRequestI } from '../../interfaces/requestI';
+
+const getUserSubscriptions = async (
+  req: UserRequestI,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { id } = req.user;
+    const userSubscriptionData = await Subscription.find({ userId: id });
+    res.status(200).json({ userSubscriptionData, msg: 'Get user subscription successfully!' });
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    next(new CustomError(500, error.message));
+  }
+};
+
+export default getUserSubscriptions;
