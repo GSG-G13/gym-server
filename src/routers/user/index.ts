a2180802e9e9c7437bad66fad6eaaad0d9e88273
AhmedMughessib <amoghaseeb@gmail.com>
2023-06-18 14:41:18 +0300

    authorization check

--- src/routers/user/index.ts
+++ src/routers/user/index.ts
@@ -3,10 +3,12 @@ import signup from '../../controllers/user/signup';
 import validationData from '../../middlewares/validation';
 import { signupValidationSchema } from '../../validations/index';
 import getAllUsers from '../../controllers/user/getallusers';
+import checkAuth from '../../middlewares/checkAuth';
+import checkRole from '../../middlewares/checkRole';
 
 const userRouter: Router = express.Router();
 
 userRouter.post('/', validationData(signupValidationSchema), signup);
-userRouter.get('/', getAllUsers);
+userRouter.get('/', checkRole, getAllUsers);
 
 export default userRouter;
