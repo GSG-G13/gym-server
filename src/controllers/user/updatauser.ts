b9ddab7b47fcb1819e6c717c7858c067a3430df8
AhmedMughessib <amoghaseeb@gmail.com>
2023-06-19 11:42:51 +0300

    remove comments and unnecesary code

--- src/controllers/user/updatauser.ts
+++ src/controllers/user/updatauser.ts
@@ -1,18 +1,15 @@
 import { NextFunction, Response } from 'express';
-import { hash } from 'bcrypt';
 import User from '../../database/user';
 import { TokenRequest } from '../../middlewares/checkAuth';
 import CustomError from '../../helpers';
 
 const updateUser = async (req: TokenRequest, res: Response, next: NextFunction): Promise<void> => {
   try {
-    const { username, email, password, weight, height, gender, goalweight } = req.body;
+    const { username, email, weight, height, gender, goalweight } = req.body;
     const { id } = req.user;
-    const hashedPasswod = await hash(password, 10);
     await User.updateOne({ _id: id }, {
       username,
       email,
-      password: hashedPasswod,
       weight,
       height,
       gender,
