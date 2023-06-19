cf4a1e23d17d4dc2aa5df5ed8938d8576d162339
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-06-19 13:27:20 +0300

    get user orders Relates #97

---
+++ src/controllers/orders/getUserOrders.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Response } from 'express';
+import Order from '../../database/order';
+import { TokenRequest } from '../../middlewares/checkAuth';
+import CustomError from '../../helpers';
+
+const getUserOrders = async (
+  req:TokenRequest,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { id } = req.user;
+    const orders = await Order.find({ userId: id });
+    res.status(201).json({
+      orders,
+      msg: 'get user orders successfully',
+    });
+  } catch (error) {
+    next(new CustomError(500, error.message));
+  }
+};
+
+export default getUserOrders;
