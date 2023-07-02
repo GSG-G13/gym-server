b14108b84dbcec2b0774d45a93dd51cff629c56a
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-07-02 15:48:32 +0300

    add main endpoint Relates #103

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,6 @@ const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(router);
+app.use('/api', router);
 
 export default app;
