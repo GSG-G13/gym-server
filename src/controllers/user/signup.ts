dad94e41f17282477b5fdbca5eda5e36e079812a
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-07-16 21:30:21 +0300

    delete send cookie while sigin up and other Relates #153

--- src/controllers/user/signup.ts
+++ src/controllers/user/signup.ts
@@ -1,9 +1,7 @@
 import type { NextFunction, Request, Response } from 'express';
 import { hash } from 'bcrypt';
-import { sign } from 'jsonwebtoken';
 import User from '../../database/user';
 import CustomError from '../../helpers';
-import { SECRET_KEY } from '../../config';
 
 const signup = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
@@ -15,7 +13,7 @@ const signup = async (req: Request, res: Response, next: NextFunction): Promise<
       throw new CustomError(400, 'Email already exist');
     } else {
       const hashedPassword = await hash(password, 10);
-      const userData = await User.create({
+      await User.create({
         username,
         email,
         password: hashedPassword,
@@ -27,12 +25,7 @@ const signup = async (req: Request, res: Response, next: NextFunction): Promise<
         role,
       });
 
-      const token = sign({
-        username: userData.username,
-        id: userData._id,
-        role: userData.role,
-      }, SECRET_KEY);
-      res.cookie('token', token).status(201).json({ massage: 'user created successfully' });
+      res.status(201).json({ massage: 'user created successfully' });
     }
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
